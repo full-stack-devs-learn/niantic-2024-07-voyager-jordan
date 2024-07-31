8d314ae0b7acbc9b4351c8890a1483154e1fffff
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-07-31 08:54:55 -0700

    Refactored unnecessary code

--- sprint-1/07-intro-to-databases-select/exercise_intro-to-databases-select/11_Products_Dairy_Stock.sql
+++ sprint-1/07-intro-to-databases-select/exercise_intro-to-databases-select/11_Products_Dairy_Stock.sql
@@ -15,7 +15,7 @@ USE northwind;
 SELECT product_name
 	, unit_price
     , units_in_stock + units_on_order AS 'items_available'
+    , c.category_name
 FROM products p
 JOIN categories c
-ON p.category_id = c.category_id
 WHERE c.category_name LIKE '%Dairy%';
