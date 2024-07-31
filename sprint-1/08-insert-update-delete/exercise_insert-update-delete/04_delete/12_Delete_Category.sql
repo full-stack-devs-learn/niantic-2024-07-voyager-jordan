ce63e3ae54c9e094832ca3f0ab9342c43a2c1209
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-07-31 15:54:05 -0700

    Finished all Database exercises

--- sprint-1/08-insert-update-delete/exercise_insert-update-delete/04_delete/12_Delete_Category.sql
+++ sprint-1/08-insert-update-delete/exercise_insert-update-delete/04_delete/12_Delete_Category.sql
@@ -12,4 +12,30 @@ USE northwind;
 -- The only user input should be a variable at the top of the 
 -- script that holds the Category Name.
 -- The sript should do the rest
+SELECT * FROM order_details;
+SELECT * FROM products;
+SELECT * FROM categories;
 
+-- DELETE ORDER-DETAILS
+DELETE FROM order_details
+WHERE product_id IN (
+	SELECT product_id
+    FROM products p
+    WHERE p.category_id IN (
+		SELECT category_id
+        FROM categories
+        WHERE category_name = 'Sporting Goods'
+    )
+);
+
+DELETE FROM products p
+    WHERE p.category_id IN (
+		SELECT category_id
+        FROM categories
+        WHERE category_name = 'Sporting Goods'
+);
+
+DELETE FROM categories
+WHERE category_name = 'Sporting Goods';
+
+SELECT * FROM categories;
