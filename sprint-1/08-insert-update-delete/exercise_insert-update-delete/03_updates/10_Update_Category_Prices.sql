5d8ad49176dc3e38f55856bfc0d07efce4e41820
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-08-01 09:16:23 -0700

    Refactored Code in 10

--- sprint-1/08-insert-update-delete/exercise_insert-update-delete/03_updates/10_Update_Category_Prices.sql
+++ sprint-1/08-insert-update-delete/exercise_insert-update-delete/03_updates/10_Update_Category_Prices.sql
@@ -2,13 +2,11 @@ USE northwind;
 
 -- Write a script that updates the price of all products 
 -- within a category by a certain percent.
-SET @category_to_update = 'Beverages';
-
-SET @category_id = (SELECT category_id FROM categories WHERE category_name = @category_to_update);
-SET @percent_change = 50; -- INSERT PERCENT 50%
+SET @category_to_update = 'Beverages'; -- SET CATEGORY NAME
+SET @percent_change = 50; -- SET INCREASE/DECREASE IN PERCENT 50%
 
 UPDATE products
 SET unit_price = ROUND(unit_price + (unit_price * (@percent_change / 100)), 2)
-WHERE category_id = @category_id;
+WHERE category_id = (SELECT category_id FROM categories WHERE category_name = @category_to_update);
 
 SELECT * FROM products;
