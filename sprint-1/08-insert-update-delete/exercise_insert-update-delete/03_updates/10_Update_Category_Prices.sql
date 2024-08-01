0007093bbdc93ac38acbf90360bb87842efc84be
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-08-01 09:21:06 -0700

    Refactored Code in 10

--- sprint-1/08-insert-update-delete/exercise_insert-update-delete/03_updates/10_Update_Category_Prices.sql
+++ sprint-1/08-insert-update-delete/exercise_insert-update-delete/03_updates/10_Update_Category_Prices.sql
@@ -7,6 +7,6 @@ SET @percent_change = 50; -- SET INCREASE/DECREASE IN PERCENT 50%
 
 UPDATE products
 SET unit_price = ROUND(unit_price + (unit_price * (@percent_change / 100)), 2)
-WHERE category_id = (SELECT category_id FROM categories WHERE category_name = @category_to_update);
+WHERE category_id IN (SELECT category_id FROM categories WHERE category_name = @category_to_update);
 
 SELECT * FROM products;
