614952b8d994fa2655011ba8855bb70a4ce4b13f
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-08-02 15:41:28 -0700

    Completed 2 scripts to calculate percentage prepped for pie charts

--- sprint-1/09-database-design/exercise_database-design/BudgetManger Project/sub_categories_by_category.sql
+++ sprint-1/09-database-design/exercise_database-design/BudgetManger Project/sub_categories_by_category.sql
@@ -9,21 +9,17 @@
 USE financial;
 
 SET @search_for_user = 'John Doe', -- INSERT FULL NAME
-	@search_for_month = '08',      -- INSERT MONTH NUMBER ** ONLY AUGUST / SEPTEMBER SEEDED **
+	@search_for_month = '09',      -- INSERT MONTH NUMBER ** ONLY AUGUST / SEPTEMBER SEEDED **
     @search_for_category = 'Vacation/Travel'; -- INSERT CATEGORY NAME
     
     
-SELECT 
-	c.category_name
-	, s.sub_category_name
-	, t.amount
-    , t.date
-    , t.note
+SELECT t.amount, u.full_name
+, s.subcategory_name
+, c.category_name
 FROM transactions t
-JOIN sub_categories s ON t.category_id = s.sub_category_id
-JOIN categories c ON c.category_id = s.parent_id
-JOIN users u ON u.user_id = t.user_id
-WHERE u.full_name = @search_for_user 
-AND DATE_FORMAT(t.date, '%m') = @search_for_month 
+JOIN users u ON t.owner = u.id
+JOIN subcategories s ON t.subcategory_id = s.id
+JOIN categories c ON s.parent_id = c.id
+WHERE u.full_name = @search_for_user
+AND DATE_FORMAT(t.date, '%m') = @search_for_month
 AND c.category_name = @search_for_category
-ORDER BY DATE;
