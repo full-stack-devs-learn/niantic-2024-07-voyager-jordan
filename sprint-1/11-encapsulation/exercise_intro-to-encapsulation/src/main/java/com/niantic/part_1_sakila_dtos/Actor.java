40200c8cd89b24da8e22e1bb6b6fd2f98e5da1f4
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-08-05 16:29:31 -0700

    Completed encapsulation Exercises and Challenges

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -2,4 +2,49 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Actor
 {
+    //Variables
+    private int actorId;
+    private String first_name;
+    private String last_name;
+
+    //Default Constructor
+    public Actor(){
+
+    }
+    //Parameterized Constructor
+    public Actor(int id, String first_name, String last_name){
+        this.actorId = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+
+    }
+
+    //Getters & Setters
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFullName(){
+        return first_name + " " + last_name;
+    }
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
@@ -2,4 +2,61 @@ package com.niantic.part_3_challenge;
 
 public class Refrigerator
 {
+    private final int MAX_CAPACITY;
+    private int currentTemperature;
+    private int available;
+    private boolean isDoorOpen;
+
+    public Refrigerator(int currentTemperature, int MAX_CAPACITY){
+        this.currentTemperature = currentTemperature;
+        this.MAX_CAPACITY = MAX_CAPACITY;
+        this.available = MAX_CAPACITY;
+    }
+
+    public int getMaxCapacity(){
+        return MAX_CAPACITY;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public int setCurrentTemperature(int currentTemperature) {
+        return this.currentTemperature = currentTemperature;
+    }
+
+    public int getAvailableCapacity() {
+        return available;
+    }
+
+    public int changeAvailableCapacity(int available) {
+        return this.available = available;
+    }
+
+    public boolean isDoorOpen(){
+        return this.isDoorOpen;
+    }
+    public boolean openDoor(){
+         return isDoorOpen = true;
+    }
+    public boolean closeDoor(){
+        return isDoorOpen = false;
+    }
+
+    public boolean addItem(int numberOfItems){
+     
+        if(isDoorOpen && available - numberOfItems >= 0){
+            this.changeAvailableCapacity(this.available - numberOfItems);
+            return true;
+        }
+        return false;
+    }
+    public boolean removeItem(int numberOfItems){
+        if(isDoorOpen){
+            this.changeAvailableCapacity(this.available + numberOfItems);
+            return true;
+        }
+        return false;
+    }
+
 }
