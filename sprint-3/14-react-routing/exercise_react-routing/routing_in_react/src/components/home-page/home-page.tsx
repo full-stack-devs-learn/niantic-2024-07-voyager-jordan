f48855e16bc9b2f2c455cfdf2485787aa1854b22
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-10-01 15:02:39 -0700

    quickly styled home page

--- sprint-3/14-react-routing/exercise_react-routing/routing_in_react/src/components/home-page/home-page.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/routing_in_react/src/components/home-page/home-page.tsx
@@ -1,10 +1,25 @@
+import "./styles/home-page.css"
+import { Link } from "react-router-dom"
+
 export default function Home_Page()
 {
     return (
         <>
-        <main>
-            <h1>Home Page</h1>
-        </main>
+            <p>Welcome</p>
+            <main id="home-main">
+                <Link to="/categories">
+                    <div>
+                        <span className="material-symbols-outlined">subject</span>
+                        <p>Categories</p> 
+                    </div>
+                </Link>
+                <Link to="/products">
+                    <div>
+                        <span className="material-symbols-outlined">list</span>
+                        <p>Products</p>
+                    </div>
+                </Link>
+            </main>
         </>
     )
 }
